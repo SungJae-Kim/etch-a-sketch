d34b0c25fdcf298f47f237b6b3e7dbdd295ba2b6
SungJae-Kim <10572483+SungJae-Kim@users.noreply.github.com>
2024-10-29 19:43:10 +0000

    uhh

---
+++ etch.js
@@ -0,0 +1,14 @@
+
+
+body = document.querySelector("body");
+
+for (let i = 0; i < 16; i++) {
+    i = document.createElement("div");
+    let lineid = ""
+    lineid += i;
+    body.appendchild()
+    for (let j = 0; j < 16; j++) {
+        j = document.createElement("span");
+        i.appendChild(j);
+    }
+}
