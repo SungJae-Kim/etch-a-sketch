93101bbe0b2e8858041b5f530236f4bba90bd2d0
skimpaint <10572483+skimpaint@users.noreply.github.com>
2024-11-02 19:54:03 -0400

    oh boy starting to see why this is difficult. might start over and plan better

--- etch.js
+++ etch.js
@@ -1,26 +1,26 @@
 const main = document.querySelector(".flex-container");
-let gridSize = 16;
-gridSize = 4;
-const gridArea = gridSize * gridSize;
-const boxSize = (720 / gridSize).toString() + "px";
 
-function makegrid() {
+function makegrid(gridSize) {
+    const gridArea = gridSize * gridSize;
+    const boxSize = (720 / gridSize).toString() + "px";
     for (let i = 0; i < gridArea; i++) {
         let gridbox = document.createElement("div");
         gridbox.classList.add("box");
         gridbox.textContent = (i+1).toString();
         main.appendChild(gridbox);
     };
+    let box = document.querySelectorAll(".box");
+
+    for (let i = 0; i < box.length; i++) {
+        let rgbValue = Math.round(i / box.length * 256).toString();
+        let rgb = "rgb(" + rgbValue + ", " + rgbValue + ", " + rgbValue + ")";
+        box[i].style.backgroundColor = rgb;
+        box[i].style.height = boxSize;
+        box[i].style.width = boxSize;
+    }
 };
 
-makegrid();
+makegrid(16);
 
-let box = document.querySelectorAll(".box");
 
-for (let i = 0; i < box.length; i++) {
-    let rgbValue = Math.round(i / box.length * 256).toString();
-    let rgb = "rgb(" + rgbValue + ", " + rgbValue + ", " + rgbValue + ")";
-    box[i].style.backgroundColor = rgb;
-    box[i].style.height = boxSize;
-    box[i].style.width = boxSize;
-}
+
