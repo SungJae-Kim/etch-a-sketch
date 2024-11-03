e5a23831d0eafce2a85472ace6f3c16ac4d3c2a9
skimpaint <10572483+skimpaint@users.noreply.github.com>
2024-11-02 20:16:32 -0400

    added styles to squares as they were created. looked at someone else doing the thing... feels like cheating

--- etch.js
+++ etch.js
@@ -1,26 +1,22 @@
 const main = document.querySelector(".flex-container");
 
-function makegrid(gridSize) {
+function makeGrid(gridSize) {
+
+    let box = document.querySelectorAll(".box");
+    box.forEach((box) => box.remove());
     const gridArea = gridSize * gridSize;
     const boxSize = (720 / gridSize).toString() + "px";
     for (let i = 0; i < gridArea; i++) {
         let gridbox = document.createElement("div");
         gridbox.classList.add("box");
         gridbox.textContent = (i+1).toString();
+        gridbox.style.height = boxSize;
+        gridbox.style.width = boxSize;
         main.appendChild(gridbox);
     };
-    let box = document.querySelectorAll(".box");
-
-    for (let i = 0; i < box.length; i++) {
-        let rgbValue = Math.round(i / box.length * 256).toString();
-        let rgb = "rgb(" + rgbValue + ", " + rgbValue + ", " + rgbValue + ")";
-        box[i].style.backgroundColor = rgb;
-        box[i].style.height = boxSize;
-        box[i].style.width = boxSize;
-    }
 };
 
-makegrid(16);
+makeGrid(16);
 
 
 
