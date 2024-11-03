488d8badc2607bdae09cdc78b78576630b8dd145
skimpaint <10572483+skimpaint@users.noreply.github.com>
2024-11-02 23:24:43 -0400

    done, feels like I've cheated though

--- etch.js
+++ etch.js
@@ -1,20 +1,29 @@
 const main = document.querySelector(".flex-container");
 
-function makeGrid(gridSize) {
+const header = document.querySelector("#button-container");
+const sizeInput = document.createElement("input");
+sizeInput.setAttribute("onchange", "makeGrid(this.value)");
+const sizeButton = document.createElement("button");
+sizeButton.textContent = "size button js"
+header.appendChild(sizeInput);
+header.appendChild(sizeButton);
 
+
+function makeGrid(gridSize) {
     let box = document.querySelectorAll(".box");
     box.forEach((box) => box.remove());
     const gridArea = gridSize * gridSize;
-    const boxSize = (720 / gridSize).toString() + "px";
+    const boxSize = (800 / gridSize).toString() + "px";
     for (let i = 0; i < gridArea; i++) {
-        let gridbox = document.createElement("div");
-        gridbox.classList.add("box");
-        gridbox.textContent = (i+1).toString();
-        gridbox.style.height = boxSize;
-        gridbox.style.width = boxSize;
-        main.appendChild(gridbox);
-    };
-};
+        let newbox = document.createElement("div");
+        newbox.classList.add("box");
+        newbox.textContent = (i+1).toString();
+        newbox.style.height = boxSize;
+        newbox.style.width = boxSize;
+        newbox.addEventListener("mouseover", (event) => {newbox.style.backgroundColor = "green";})
+        main.appendChild(newbox);
+    }
+}
 
 makeGrid(16);
 
